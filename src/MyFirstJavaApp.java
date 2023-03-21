ea51796f651d7a4f7240fd461a76a61ac2af9f0f
Alex Baker <alexbaker12161995@outlook.com>
2023-03-21 15:46:39 -0500

    Added ObjectReferenceApp.java

---
+++ src/MyFirstJavaApp.java
@@ -0,0 +1,6 @@
+public class MyFirstJavaApp {
+    public static void main(String[] args) {
+        String name = "Alex";
+        System.out.println("Hello my name is " + name + "!");
+    }
+}
---
+++ src/ObjectReferenceApp.java
@@ -0,0 +1,26 @@
+public class ObjectReferenceApp {
+    public static void main(String[] args) {
+        Byte numberOfSeats = 5;
+        Short horsePower = 392;
+        Integer price = 14999;
+        Long registrationNumber = 34507897358543859L;
+        Float fuelConsumptionCombined = 15.5F;
+        Double fuelConsumptionPrecise = 15.4478383475835763892074D;
+        Boolean isDamaged = true;
+        Character energyEfficiencyCategory = 'G';
+        String carModel = "Dodge Challenger SRT 392";
+        String carModel2 = new String("Dodge Challenger SRT 392");
+
+        System.out.println(numberOfSeats);
+        System.out.println(horsePower);
+        System.out.println(price);
+        System.out.println(registrationNumber);
+        System.out.println(fuelConsumptionCombined);
+        System.out.println(fuelConsumptionPrecise);
+        System.out.println(isDamaged);
+        System.out.println(energyEfficiencyCategory);
+        System.out.println(carModel);
+        System.out.println(carModel2);
+
+    }
+}
